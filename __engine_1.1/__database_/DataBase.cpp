78a05bf254f3dfc43ef8ae49f38b0c2a97147548
Oleg Oksenchuk <oleg.oksenchuk@mail.ru>
2020-11-20 23:16:44 +0300

    Added test for twice including of obj file name. Now DataBase save array of models. Model contains all need data: Vertexs, normals.... It also contains pointers to arrays in global gpu memory space.

--- __engine_1.1/__database_/DataBase.cpp
+++ __engine_1.1/__database_/DataBase.cpp
@@ -4,10 +4,9 @@
 const std::string DataBase::kDefaultObjFilesDir = "";
 
 DataBase::DataBase()noexcept(true):
-	data_{},
+	models_{},
 	obj_files_list_{},
-	obj_files_dir_{ kDefaultObjFilesDir }
-{}
+	obj_files_dir_{ kDefaultObjFilesDir }{}
 
 void DataBase::LoadObjFiles()noexcept(true){
 
@@ -22,39 +21,33 @@ void DataBase::LoadObjFiles()noexcept(true){
 void DataBase::LoadObjFile(const std::string& file_name)noexcept(true) {
 
 	ObjFile obj_file{ file_name, obj_files_dir_ };
+
 	obj_file.Open();
 	obj_file.Read();
 
-	data_.Data<Vertex3D>().Concat(obj_file.Data<Vertex3D>());
-	data_.Data<Normal3D>().Concat(obj_file.Data<Normal3D>());
-	data_.Data<Polygon3D>().Concat(obj_file.Data<Polygon3D>());
-	data_.Data<RgbColor>().Concat(obj_file.Data<RgbColor>());
-	//...
-	
+	Model model{ obj_file.DataMove() };
+	models_.PushBack(std::move(model));
+
 	obj_file.Close();
-
-	//data_.models.PushBack();
-
-	//longSize newSizeOfArrayOfModels = this->data_info_.numberOfModels + 1;
-	//ModelInfo3D* newArrayOfModels = (ModelInfo3D*) new ModelInfo3D[newSizeOfArrayOfModels];
-	//memcpy(newArrayOfModels, this->data_info_.allModels, this->data_info_.numberOfModels * sizeof(ModelInfo3D) );
-	//newArrayOfModels[newSizeOfArrayOfModels - 1].file_name = file_name;
-	//newArrayOfModels[newSizeOfArrayOfModels - 1].numberOfNormals = obj_file.GetNumberOfNormals();
-	//newArrayOfModels[newSizeOfArrayOfModels - 1].numberOfPolygons = obj_file.GetNumberOfPolygons();
-	//newArrayOfModels[newSizeOfArrayOfModels - 1].numberOfVertexs = obj_file.GetNumberOfVertexs();
-	//newArrayOfModels[newSizeOfArrayOfModels - 1].numberOfColors = obj_file.GetNumberOfRgbColors();
-
-	//newArrayOfModels[newSizeOfArrayOfModels - 1].model_name = nullptr; //<<<<<<<<<<<<<<<<<<<<<<<<<<<
-	//newArrayOfModels[newSizeOfArrayOfModels - 1].vertexs_pointer = data_info_.allVertexs + data_info_.numberOfVertexs - obj_file.GetNumberOfVertexs();
-	//newArrayOfModels[newSizeOfArrayOfModels - 1].normals_pointer = data_info_.allNormals + data_info_.numberOfNormals - obj_file.GetNumberOfNormals();
-	//newArrayOfModels[newSizeOfArrayOfModels - 1].polygons_pointer = data_info_.allPolygons + data_info_.numberOfPolygons - obj_file.GetNumberOfPolygons();
-	//newArrayOfModels[newSizeOfArrayOfModels - 1].colors_pointer = data_info_.allRgbColors + data_info_.numberOfRgbColors - obj_file.GetNumberOfRgbColors();
-	//
-	//delete[] this->data_info_.allModels;
-	//this->data_info_.allModels = newArrayOfModels;
-	//this->data_info_.numberOfModels++;
-
 
 }
 
+void DataBase::IncludeObjFileName(const std::initializer_list<std::string>& list)const noexcept(true) {
+
+	for (auto& obj : list) {
+
+		//Twice including test
+		auto name_it = std::find(obj_files_list_.cbegin(), obj_files_list_.cend(), obj);
+		bool list_contains_name_yet = !(name_it == obj_files_list_.cend());
+
+		if (!list_contains_name_yet)
+			obj_files_list_.push_back(obj);
+
+	}
+
+}
+
+void DataBase::OutputModelsInfo()noexcept(true){
+
+}
 
--- __engine_1.1/__database_/DataBase.hpp
+++ __engine_1.1/__database_/DataBase.hpp
@@ -2,49 +2,67 @@
 #ifndef _DATABASE_HPP_
 #define _DATABASE_HPP_
 
+#include <algorithm>
 #include "__file_formats_/Obj.hpp"
-#include "../engine_types.hpp"
 
 using ObjFilesList = std::vector<std::string>;
 
 class DataBase final{
 private:
 
+	//Default directory with object files(.obj)
 	static const std::string kDefaultObjFilesDir;
-	DataHost data_;
+	//All information about models components: vertexs, normals, polygons, rgb_colors
+	ArrayOf<Model> models_;
+	//All models information
+	//List with included names of obj files, that will be include
 	mutable ObjFilesList obj_files_list_;
+	//Current obj files directory
 	mutable std::string obj_files_dir_;
+	//Load info from one object file
 	void LoadObjFile(const std::string& file_name)noexcept(true);
 
 public:
 
+	//Default constructor
 	explicit DataBase()noexcept(true);
+
+	//Load ALL obj files, which names included in obj_files_list_
 	void LoadObjFiles()noexcept(true);
 
+	//Delete operators and constructors
 	DataBase& operator=(DataBase&&)noexcept(true) = delete;
 	DataBase& operator=(const DataBase&)noexcept(true) = delete;
 	explicit DataBase(DataBase&&)noexcept(true) = delete;
 	explicit DataBase(const DataBase&)noexcept(true) = delete;
 
-	inline const DataHost& CopyData()const noexcept(true){ return data_; };
-	inline DataHost&& MoveData()noexcept(true){ return std::move(data_); };
+	inline const Model& operator[](const size_t index)const noexcept(true) { return models_[index]; }
+	inline Model& operator[](const size_t index)noexcept(true) { return models_[index]; }
 
+	inline size_t ModelsNumber()const noexcept(true) { models_.Size(); };
+	//Move all data from DataBase, after that operation DataBase will be empty
+
+	//Set directory where will be find obj files
 	inline void ObjFilesDir(const std::string& dir_path)const noexcept(true) {
 
 		obj_files_dir_ = dir_path;
 
 	};
 
-	inline void IncludeObjFileName(const std::initializer_list<std::string>& list)const noexcept(true){
+	/*inline DataBase& operator+(ObjFile& obj_file)noexcept(true) {
 
-		for (auto& obj : list)
-			obj_files_list_.push_back(obj);
-		
-	}
+		data_ = data_ + obj_file.Data();
+
+	}*/
+
+	//Include obj files name to obj_files_list
+	void IncludeObjFileName(const std::initializer_list<std::string>& list)const noexcept(true);
 
 	//virtual formal
 	virtual ~DataBase() {}
 
+	void OutputModelsInfo()noexcept(true);
+
 };
 
-#endif _DATABASE_HPP_
+#endif //_DATABASE_HPP_
