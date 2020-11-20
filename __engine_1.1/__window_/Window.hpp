4e1df213474190d6f2c70427ade7de58b12227b4
Oleg Oksenchuk <oleg.oksenchuk@mail.ru>
2020-11-20 23:23:55 +0300

    Delete some useless defines, minimal changes.

--- __engine_1.1/__window_/Window.hpp
+++ __engine_1.1/__window_/Window.hpp
@@ -6,29 +6,23 @@
 #include <Windows.h>
 #include "../engine_types.hpp"
 
-enum SizeMode {
-
-	SIZEBLE,
-	NOT_SIZEBLE,
-	FULL_SCREEN
-
-};
-
-#define VK_A 65
-#define VK_B 66
-#define VK_D 68
-#define VK_W 87
-#define VK_S 83
+//enum SizeMode {
+//
+//	SIZEBLE,
+//	NOT_SIZEBLE,
+//	FULL_SCREEN
+//
+//};
+//
+//#define VK_A 65
+//#define VK_B 66
+//#define VK_D 68
+//#define VK_W 87
+//#define VK_S 83
 
 class Window final{
 private:
 
-	//Window class constants
-	static const std::wstring kDefaultTitle;
-	static const std::size_t kDefaultWidth;
-	static const std::size_t kDefaultHeight;
-	static const std::size_t kDefaultX;
-	static const std::size_t kDefaultY;
 	static const std::wstring kClassName;
 
 	std::wstring title_;
@@ -39,22 +33,27 @@ private:
 	size_t height_;
 	size_t x_;
 	size_t y_;
-	KeystrokesQueue keystroke_queue_;
+	KeystrokesQueue keystrokes_queue_;
 
 
-			//RgbPixel* display_buffer_;
-			//RgbPixel* now_is_displaying_buffer_;
-			//DISPLAYBUFFERINFO display_buffer_info_;
-			//const unsigned int kBitsPerPixel = 32;
-			//win_coordinats win_coordinats_;
+	//RgbPixel* display_buffer_;
+	//RgbPixel* now_is_displaying_buffer_;
+	//DISPLAYBUFFERINFO display_buffer_info_;
+	//const unsigned int kBitsPerPixel = 32;
+	//win_coordinats win_coordinats_;
 
+	//high_resolution_clock::time_point first_time_point_count_fps_;
 
-			//SizeMode sizeMode;
-			//high_resolution_clock::time_point first_time_point_count_fps_;
-			//std::vector<Keystroke> keys_;
-			//static void PushKeystroke(KeyType key);
+	//static void PushKeystroke(KeyType key);
 
 public:
+
+	//Window class constants
+	static const std::wstring kDefaultTitle;
+	static const std::size_t kDefaultWidth;
+	static const std::size_t kDefaultHeight;
+	static const std::size_t kDefaultX;
+	static const std::size_t kDefaultY;
 
 	static LRESULT CALLBACK WndProc(HWND hWnd, UINT Message, WPARAM  wParam, LPARAM lParam);
 	void PlugHInstance(const HINSTANCE hInstance)const noexcept(true);
@@ -72,11 +71,6 @@ public:
 	WPARAM StartMessageLoop()const noexcept(true);
 	void RegisterWindowClass()noexcept(true);
 
-			//Size GetSize();
-			//HDC GetDeviceContext();
-			//HWND GetHandle();
-			//win_coordinats GetCoord();
-			//void SetSizeMode(SizeMode sizeMode) { this->sizeMode = sizeMode; };
 };
 
 #endif // !_WINDOW_HPP_
