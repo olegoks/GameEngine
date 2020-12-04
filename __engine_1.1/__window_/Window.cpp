c3299292d63e14840165bac37698cc71dd44c0d0
Oleg Oksenchuk <oleg.oksenchuk@mail.ru>
2020-12-05 01:30:44 +0300

    Added keystrokes processing and KeystrokesQueue.

--- __engine_1.1/__window_/Window.cpp
+++ __engine_1.1/__window_/Window.cpp
@@ -11,125 +11,127 @@ const std::size_t Window::kDefaultHeight{ 1080 };
 const std::size_t Window::kDefaultX{ 0 };
 const std::size_t Window::kDefaultY{ 0 };
 const std::wstring Window::kClassName{ L"Main window class name." };
-//		
-//void Window::PushKeystroke(KeyType key) {
-//
-//	using namespace std::chrono;
-//	high_resolution_clock::time_point now_time_point = high_resolution_clock::now();
-//	unsigned int now = duration_cast<milliseconds>(high_resolution_clock::now().time_since_epoch()).count();
-//	const Keystroke keystroke = { key, now };
-//	This->keys_.push_back(keystroke);
-//
-//}
 
-Window::Window()noexcept(true) :
-	hInstance_{ 0 },
-	title_{ kDefaultTitle },
-	width_{ kDefaultWidth },
-	height_{ kDefaultHeight },
-	x_{ kDefaultX },
-	y_{ kDefaultY }{}
+Window::Window()noexcept(true):
+	hInstance_{ nullptr },
+	params_{ kDefaultWidth, 
+	kDefaultHeight, 
+	kDefaultX, 
+	kDefaultY, 
+	kDefaultTitle },
+	device_context_{ nullptr },
+	self_hWnd_{ nullptr }{}
 
-		LRESULT CALLBACK Window:: WndProc(HWND hWnd, UINT Message, WPARAM  wParam, LPARAM lParam) {
 
-			Window* wnd_ptr = reinterpret_cast<Window*>(GetWindowLong(hWnd, 0));
+void Window::PushKeystroke(KeyType key)noexcept(true) {
 
-			switch (Message) {
+	using namespace std::chrono;
+	high_resolution_clock::time_point now_time_point = high_resolution_clock::now();
+	long long  now = duration_cast<milliseconds>(high_resolution_clock::now().time_since_epoch()).count();
+	const Keystroke keystroke = Keystroke{ key, now };
+	keystrokes_queue_.Push(keystroke);
 
-			case WM_KEYDOWN: {
+}
 
-				const unsigned int key = wParam;
+LRESULT CALLBACK Window:: WndProc(HWND hWnd, UINT Message, WPARAM  wParam, LPARAM lParam) {
 
-				/*switch (key) {
+	Window* this_wnd = reinterpret_cast<Window*>(GetWindowLong(hWnd, 0));
 
-				case VK_UP: { PushKeystroke(KeyType::ArrowUp);break;}
-				case VK_DOWN: { PushKeystroke(KeyType::ArrowDown);break;}
-				case VK_LEFT: { PushKeystroke(ArrowLeft);break;};
-				case VK_RIGHT: { PushKeystroke(ArrowRight);break;}
-				case VK_A: { PushKeystroke(A); break; };
-				case VK_D: { PushKeystroke(D); break; };
-				case VK_S: { PushKeystroke(S); break; };
-				case VK_W: { PushKeystroke(W); break; };
+	switch (Message) {
 
-				}*/
-				
-			break;
+	case WM_KEYDOWN: {
+
+		const unsigned int key = wParam;
+
+		switch (key) {
+
+			case VK_UP: { this_wnd->PushKeystroke(KeyType::ArrowUp); break; }
+			case VK_DOWN: { this_wnd->PushKeystroke(KeyType::ArrowDown); break; }
+			case VK_LEFT: { this_wnd->PushKeystroke(KeyType::ArrowLeft); break; };
+			case VK_RIGHT: { this_wnd->PushKeystroke(KeyType::ArrowRight); break; }
+			case VK_A: { this_wnd->PushKeystroke(KeyType::A); break; };
+			case VK_D: { this_wnd->PushKeystroke(KeyType::D); break; };
+			case VK_S: { this_wnd->PushKeystroke(KeyType::S); break; };
+			case VK_W: { this_wnd->PushKeystroke(KeyType::W); break; };
+
+		}
+		break;
 			
-			}
+	}
 
-			case WM_MOUSEWHEEL: {
+	case WM_MOUSEWHEEL: {
 
-				static int wheel_delta = 0;
-				wheel_delta += GET_WHEEL_DELTA_WPARAM(wParam);
+		static int wheel_delta = 0;
+		wheel_delta += GET_WHEEL_DELTA_WPARAM(wParam);
 				
-				while (wheel_delta > WHEEL_DELTA) {
+		while (wheel_delta > WHEEL_DELTA) {
 					//PushKeystroke(WheelUp);
-					wheel_delta -= WHEEL_DELTA;
-				}
+			wheel_delta -= WHEEL_DELTA;
+		}
 				
-				while (wheel_delta < 0) {
+		while (wheel_delta < 0) {
 					//PushKeystroke(WheelDown);
-					wheel_delta += WHEEL_DELTA;
-				}
+			wheel_delta += WHEEL_DELTA;
+		}
 
-				break;
-			}
+		break;
+	}
 
-			case WM_CLOSE: {DestroyWindow(hWnd);break;}
+	case WM_CLOSE: { DestroyWindow(hWnd);break; }
 
-			case WM_DESTROY: {PostQuitMessage(0);break;}
+	case WM_DESTROY: { PostQuitMessage(0);break; }
 		
-			default: return DefWindowProc(hWnd, Message, wParam, lParam); 
+	default: return DefWindowProc(hWnd, Message, wParam, lParam); 
 		
-		}
+	}
 
-		}
+}
 
-		void Window::PlugHInstance(const HINSTANCE hInstance)const noexcept(true){
+void Window::PlugHInstance(const HINSTANCE hInstance)const noexcept(true){
 
-			if (hInstance_ == 0)
-				hInstance_ = hInstance;
+	if (hInstance_ == 0)
+		hInstance_ = hInstance;
 
-		}
+}
 		
-		Window::~Window() {}
+Window::~Window() {}
 
-		void Window::RegisterWindowClass()noexcept(true) {
+void Window::RegisterWindowClass()noexcept(true) {
 
-			WNDCLASSEX wc; //Создаем экземпляр класса WNDCLASSEX
-			wc.cbSize = sizeof(wc);
-			wc.style = CS_HREDRAW | CS_VREDRAW;
-			wc.lpfnWndProc = this->WndProc;
-			wc.cbClsExtra = 0;
-			wc.cbWndExtra = 4;
-			wc.hInstance = hInstance_;
-			wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-			wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-			wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-			wc.lpszMenuName = NULL;
-			wc.lpszClassName = kClassName.c_str();
-			wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	WNDCLASSEX wc; //Создаем экземпляр класса WNDCLASSEX
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = this->WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 4;
+	wc.hInstance = hInstance_;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = kClassName.c_str();
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
 
-			if (!RegisterClassEx(&wc)) {
+	if (!RegisterClassEx(&wc)) {
 
-				MessageBox(nullptr,
-					L"Не удалось зарегестрировать класс",
-					L"Ошибка регистрации класса!",
-					MB_OK);
+		MessageBox(nullptr,
+			L"Не удалось зарегестрировать класс",
+			L"Ошибка регистрации класса!",
+			MB_OK);
 
-			}
+	}
 
-		}
+}
 
-		void Window::Create()noexcept(true){
+void Window::Create()noexcept(true){
 			
 			self_hWnd_ = CreateWindowEx(WS_EX_TOPMOST, kClassName.c_str(),
-				title_.c_str(),
-				WS_POPUP,
-				x_,
-				y_, 
-				width_, 
-				height_, 
+				params_.title_.c_str(),
+				WS_OVERLAPPEDWINDOW,//WS_POPUP,
+				params_.x_,
+				params_.y_, 
+				params_.width_, 
+				params_.height_, 
 				HWND(NULL), 
 				HMENU(NULL), 
 				(HINSTANCE)hInstance_, 
@@ -140,7 +142,8 @@ Window::Window()noexcept(true) :
 				MessageBox(NULL, L"Не получилось создать окно!", L"Ошибка", MB_OK);
 
 			}
-			
+
+			//Что-то нужно сделать))
 			SetWindowLong(self_hWnd_, 0, (LONG)this);
 
 			SetProcessDPIAware();//Вызывая эту функцию (SetProcessDPIAware), вы сообщаете системе, что интерфейс вашего приложения умеет сам правильно масштабироваться при высоких значениях DPI (точки на дюйм). Если вы не выставите этот флаг, то интерфейс вашего приложения может выглядеть размыто при высоких значениях DPI.
@@ -194,83 +197,58 @@ Window::Window()noexcept(true) :
 
 		}
 
-		//void Window::DisplayFrame() {
-		//	
-		//	//using namespace std::chrono;
-		//	//typedef high_resolution_clock::time_point time_point;
-		//	//typedef high_resolution_clock::duration duration;
+		void Window::ShowFrame(const FrameHost& frame)noexcept(true) {
+			
+			//using namespace std::chrono;
+			//typedef high_resolution_clock::time_point time_point;
+			//typedef high_resolution_clock::duration duration;
 
-		//	//time_point second_time_point_count_fps = high_resolution_clock::now();
+			//time_point second_time_point_count_fps = high_resolution_clock::now();
 
-		//	//duration render_frame_duration = second_time_point_count_fps - first_time_point_count_fps_;
-		//	//int current_fps = 0;
-		//	//current_fps = 1000.0f / (float)duration_cast<milliseconds>(render_frame_duration).count();
+			//duration render_frame_duration = second_time_point_count_fps - first_time_point_count_fps_;
+			//int current_fps = 0;
+			//current_fps = 1000.0f / (float)duration_cast<milliseconds>(render_frame_duration).count();
 
-		//	//if (current_fps > 100) { current_fps = 99; }
-		//	//	else
-		//	//		if (current_fps < 0) { current_fps = 0; };
-		//	////fps = 30;
-		//	//char fps_char[3] = { "00" };
-		//	//_itoa(current_fps, fps_char, 10);
-		//	//wchar_t* string = ConvertString(fps_char);
+			//if (current_fps > 100) { current_fps = 99; }
+			//	else
+			//		if (current_fps < 0) { current_fps = 0; };
+			////fps = 30;
+			//char fps_char[3] = { "00" };
+			//_itoa(current_fps, fps_char, 10);
+			//wchar_t* string = ConvertString(fps_char);
 
-		//	SetDIBitsToDevice(
-		//			DeviceContext,//HDC hdc
-		//			0,//int XDest,
-		//			0,//int YDest,+
-		//			size_.width ,//DWORD dwWidth, 
-		//			size_.height ,//DWORD dwHeight,+
-		//			0,//int XSrc,
-		//			0,//int YSrc,
-		//			0,// UINT uStartScan
-		//			size_.height,//UINT cScanLines
-		//			this->display_buffer_,
-		//			&this->display_buffer_info_,
-		//		DIB_PAL_COLORS//DIB_RGB_COLORS/*,
-		//			//SRCCOPY
-		//	);
-		//	/*TextOut(DeviceContext, 5, 5, string, 2);
+			SetDIBitsToDevice(
+					device_context_,
+					0,
+					0,
+					params_.width_, 
+					params_.height_,
+					0,
+					0,
+					0,
+					params_.height_,
+					static_cast<const void*>(frame),
+					&frame.Info(),
+					DIB_PAL_COLORS
+			);
 
-		//	first_time_point_count_fps_ = high_resolution_clock::now();*/
+			/*TextOut(DeviceContext, 5, 5, string, 2);
 
-		//		
+			first_time_point_count_fps_ = high_resolution_clock::now();*/
 
-		//		//int SetDIBitsToDevice(
-		//		//	HDC hdc,                 // дескриптор DC
-		//		//	int XDest,               // x-коорд. верхнего левого угла приемника
-		//		//	int YDest,               // y-коорд. верхнего левого угла приемника
-		//		//	DWORD dwWidth,           // ширина прямоугольника источника
-		//		//	DWORD dwHeight,          // высота прямоугольника источника
-		//		//	int XSrc,                // x-коорд. нижнего левого угла источника
-		//		//	int YSrc,                // y-коорд. нижнего левого угла источника
-		//		//	UINT uStartScan,         // первая строка развертки в массиве
-		//		//	UINT cScanLines,         // число строк развертки
-		//		//	CONST VOID * lpvBits,     // массив битов DIB
-		//		//	CONST BITMAPINFO * lpbmi, // информация о точечном рисунке
-		//		//	UINT fuColorUse          // RGB или индексы палитры
-		//		//);
-		//	
-
-		//}
-
-		//void Window::SetDisplayBuffer(RgbPixel* display_buffer, const unsigned int display_buffer_size){
-		//
-		//	this->display_buffer_ = display_buffer;
-
-		//	ZeroMemory(&this->display_buffer_info_.bmiHeader, sizeof(BITMAPINFO));
-
-		//	this->display_buffer_info_.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-		//	this->display_buffer_info_.bmiHeader.biWidth = this->size_.width;
-		//	this->display_buffer_info_.bmiHeader.biHeight = -((int) (this->size_.height));
-		//	this->display_buffer_info_.bmiHeader.biPlanes = 1;
-		//	this->display_buffer_info_.bmiHeader.biBitCount = 32;//this->kBitsPerPixel;
-		//	this->display_buffer_info_.bmiHeader.biCompression = BI_RGB;
-		//	this->display_buffer_info_.bmiHeader.biSizeImage = ((1920 * 24 + 31) & ~31) / 8 * 1080;//display_buffer_size;
-		//	this->display_buffer_info_.bmiHeader.biXPelsPerMeter = 0;
-		//	this->display_buffer_info_.bmiHeader.biYPelsPerMeter = 0;
-		//	this->display_buffer_info_.bmiHeader.biClrUsed = 0;
-		//	this->display_buffer_info_.bmiHeader.biClrImportant = 0;
-		//	
-		//}
-
-		//Window* Window::This = nullptr;
+				//int SetDIBitsToDevice(
+				//	HDC hdc,                 // дескриптор DC
+				//	int XDest,               // x-коорд. верхнего левого угла приемника
+				//	int YDest,               // y-коорд. верхнего левого угла приемника
+				//	DWORD dwWidth,           // ширина прямоугольника источника
+				//	DWORD dwHeight,          // высота прямоугольника источника
+				//	int XSrc,                // x-коорд. нижнего левого угла источника
+				//	int YSrc,                // y-коорд. нижнего левого угла источника
+				//	UINT uStartScan,         // первая строка развертки в массиве
+				//	UINT cScanLines,         // число строк развертки
+				//	CONST VOID * lpvBits,     // массив битов DIB
+				//	CONST BITMAPINFO * lpbmi, // информация о точечном рисунке
+				//	UINT fuColorUse          // RGB или индексы палитры
+				//);
+			
+		}
--- __engine_1.1/__window_/Window.hpp
+++ __engine_1.1/__window_/Window.hpp
@@ -14,37 +14,33 @@
 //
 //};
 //
-//#define VK_A 65
-//#define VK_B 66
-//#define VK_D 68
-//#define VK_W 87
-//#define VK_S 83
+#define VK_A 65
+#define VK_B 66
+#define VK_D 68
+#define VK_W 87
+#define VK_S 83
+
+static struct Parametrs {
+
+	size_t width_;
+	size_t height_;
+	size_t x_;
+	size_t y_;
+	std::wstring title_;
+
+};
 
 class Window final{
 private:
 
 	static const std::wstring kClassName;
-
-	std::wstring title_;
 	HWND self_hWnd_;
 	mutable HINSTANCE hInstance_;
 	HDC device_context_;
-	size_t width_;
-	size_t height_;
-	size_t x_;
-	size_t y_;
 	KeystrokesQueue keystrokes_queue_;
+	Parametrs params_;
 
-
-	//RgbPixel* display_buffer_;
-	//RgbPixel* now_is_displaying_buffer_;
-	//DISPLAYBUFFERINFO display_buffer_info_;
-	//const unsigned int kBitsPerPixel = 32;
-	//win_coordinats win_coordinats_;
-
-	//high_resolution_clock::time_point first_time_point_count_fps_;
-
-	//static void PushKeystroke(KeyType key);
+	void PushKeystroke(KeyType key)noexcept(true);
 
 public:
 
@@ -59,18 +55,17 @@ public:
 	void PlugHInstance(const HINSTANCE hInstance)const noexcept(true);
 	explicit Window()noexcept(true);
 	~Window();
-
-			//void SetDisplayBuffer(RgbPixel* display_buffer, const unsigned int display_buffer_size);
-
-			//void DisplayFrame();
-
-			//std::vector<Keystroke>* GetKeystrokesBuffer() noexcept { return &keys_; }
-			//RgbPixel* GetHostDisplayBuffer() const noexcept { return display_buffer_; };
+	void ShowFrame(const FrameHost& frame)noexcept(true);
+	inline Keystroke PopKeystroke()noexcept(true) { keystrokes_queue_.Pop(); };
 	void Create()noexcept(true);
 	void Show()const noexcept(true);
 	WPARAM StartMessageLoop()const noexcept(true);
 	void RegisterWindowClass()noexcept(true);
-
+	inline size_t Width()const noexcept(true) { return params_.width_; };
+	inline size_t Height()const noexcept(true) { return params_.height_; };
+	inline void Width(const size_t width)noexcept(true) { params_.width_ = width; };
+	inline void Height(const size_t height)noexcept(true) { params_.height_ = height; };
+	inline KeystrokesQueue* KeystrokesQueue()noexcept(true) { return &keystrokes_queue_; };
 };
 
 #endif // !_WINDOW_HPP_
